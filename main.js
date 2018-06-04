dd7b8e4cc45ab17f9207874c5a3fa58096a76208
Joe Begley <joebegley15@gmail.com>
2018-06-04 13:38:54 -0400

    Created the object

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 function stopWatch(){
-	this.timeElapsed = 0;
+	this.duration = 0;
+	this.startTime;
+	this.stopTime;
 	this.getTime = function(){
-		return this.timeElapsed;
+		if (this.startTime) {
+			this.duration = (new Date() - this.startTime) / 1000;
+			return this.duration;
+		} else {
+			return 0;
+		}
+	}
+	this.start = function() {
+		this.startTime = new Date();
+	}
+	this.stop = function(){
+		this.stopTime = new Date();
+		this.duration = this.stopTime - this.startTime;
+		return this.duration;
+	}
+	this.reset = function(){
+		this.stopTime = undefined;
+		this.startTime = undefined;
+		this.duration = undefined;
+		return 0;
 	}
 }
+
+var myStopWatch = new stopWatch();
